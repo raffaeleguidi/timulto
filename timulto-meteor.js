b507b67d8601470f886684100dddfdddfd8a373c
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-03-23 23:33:58 +0100

    added geolocation

--- timulto-meteor.js
+++ timulto-meteor.js
@@ -10,13 +10,17 @@ if (Meteor.isClient) {
        return Tasks.find({}, {sort: {createdAt: -1}});
     }, photo: function () {
       return Session.get("photo");
-    }
+    }, loc: function () {
+      // return 0, 0 if the location isn't ready
+      return Geolocation.latLng() || { lat: 0, lng: 0 };
+    },
+    error: Geolocation.error
   });
 
   Template.body.events({
     "click #shoot": function(event) {
         event.preventDefault();
-        MeteorCamera.getPicture(function(error, data) {
+        MeteorCamera.getPicture({ width: 300, height: 300 }, function(error, data) {
            Session.set("photo", data);
         })
     },
@@ -24,10 +28,14 @@ if (Meteor.isClient) {
         event.preventDefault();
         var text = $("#description").val();
         var imageData = $("#imgdata").val();
+        var lat = $("#lat").val();
+        var lng = $("#lng").val();
 
         Tasks.insert({
           text: text,
           imageData: imageData,
+          lat: lat,
+          lng: lng,
           createdAt: new Date() // current time
         });
 
