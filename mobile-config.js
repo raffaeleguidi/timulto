a8fcd42b7f41a162c536528be1bc556a16cea768
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-03-23 23:06:39 +0100

    some cleanup and added a mobile-config.js

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'it.timulto.app',
+  name: 'timulto',
+  description: 'Delazione e senso civico',
+  author: 'TiMulto.it',
+  email: 'raffaele.p.guidi@gmail.com',
+  website: 'http://timulto.meteor.com'
+});
