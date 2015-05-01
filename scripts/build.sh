a2d0431b460f321be860e78142468adb61ad906a
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-05-01 16:38:05 +0200

    build & sign scripts parametrized

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,10 @@
-# run this script from the root of the project
+    # run this script 4from the root of the project
 #
 # ..>scripts/build.sh
 #
 # for further information: https://github.com/Differential/meteor-mobile-cookbook/blob/master/Android/Building.md
 
-meteor build --server beta.timulto.org --directory ../timulto-build --mobile-settings settings.json
+prod_domain=$1
+
+rm -rf ../timulto-build/android
+meteor build --server $prod_domain --directory ../timulto-build --mobile-settings settings.json
