ea47e97fcd91d12ffe4e51da55177781b4b02281
Manuel Morini <morinimanuel@gmail.com>
2015-05-06 12:18:54 +0200

    added sign algorithm parameter to jarsigner command

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -1,3 +1,3 @@
 rm ../timulto-build/android/timulto-release-signed.apk
-jarsigner -keystore ~/.keystore -digestalg SHA1 ../timulto-build/android/unaligned.apk timulto
+jarsigner -keystore ~/.keystore -sigalg SHA1withRSA -digestalg SHA1 ../timulto-build/android/unaligned.apk timulto
 ~/.meteor/android_bundle/android-sdk/build-tools/21.0.0/zipalign 4 ../timulto-build/android/unaligned.apk ../timulto-build/android/timulto-release-signed.apk
