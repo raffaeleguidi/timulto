d64f33e2ba22b9f9115315a2f191b59f62c52259
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-05-12 18:19:44 +0200

    added IOS icons and splashes

--- mobile-config.js
+++ mobile-config.js
@@ -17,14 +17,29 @@ App.launchScreens({
   'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
   'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
   'android_xhdpi_portrait': 'resources/splash/splash-480x800.png',
-  'android_xhdpi_landscape': 'resources/splash/splash-800x480.png'
+  'android_xhdpi_landscape': 'resources/splash/splash-800x480.png',
+
+// iOS
+  'iphone': 'resources/splash/splash-480x800.png',
+  'iphone_2x': 'resources/splash/splash-480x800.png',
+  'iphone5': 'resources/splash/splash-480x800.png',
+  'ipad_portrait': 'resources/splash/splash-480x800.png',
+  'ipad_portrait_2x': 'resources/splash/splash-480x800.png',
+  'ipad_landscape': 'resources/splash/splash-800x480.png',
+  'ipad_landscape_2x': 'resources/splash/splash-800x480.png',
 });
 
 App.icons({
   'android_ldpi': 'resources/icons/320.png',
   'android_mdpi': 'resources/icons/320.png',
   'android_hdpi': 'resources/icons/320.png',
-  'android_xhdpi': 'resources/icons/320.png'
+  'android_xhdpi': 'resources/icons/320.png',
+
+// iOS
+  'iphone': 'resources/icons/320.png',
+  'iphone_2x': 'resources/icons/320.png',
+  'ipad': 'resources/icons/320.png',
+  'ipad_2x': 'resources/icons/320.png',
 });
 
 App.accessRule('http://*.meteor.local/*');
