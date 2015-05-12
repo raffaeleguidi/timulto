63dd58565e387f1a68f245570e861697e4c32584
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-05-12 17:53:58 +0200

    moved rootUrl back to helper

--- client/lib/urlHandling.js
+++ client/lib/urlHandling.js
@@ -1,5 +1,14 @@
 function rootUrl() {
-    return Session.get("rootUrl");
+    if (Session.get("rootUrl")) {
+        return Session.get("rootUrl");
+    } else {
+        Session.set("rootUrl", "http://beta.timulto.org");
+        Meteor.call("rootUrl", function(err, res){
+            if (!err) Session.set("rootUrl", res);
+            return Session.get("rootUrl");
+        })
+        return "http://beta.timulto.org";
+    }
 }
 
 urlHandling = {
