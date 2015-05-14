8aede6ed219e92030fd7204047abf84f521d1374
Manuel Morini <morinimanuel@gmail.com>
2015-05-15 01:43:40 +0200

    forgot to commit this file related to previous commit

--- client/logic/fines.js
+++ client/logic/fines.js
@@ -1,3 +1,8 @@
+Template.fineInARow.rendered = function() {
+    var now = moment();
+//    console.log("resetting last used to " + now.toString());
+    Session.set("lastUsed", now.toString());
+};
 
 Template.fineInARow.helpers({
     imageUrl: function(){
