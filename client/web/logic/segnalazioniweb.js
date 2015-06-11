68e816c0861bc6e121bcd318ee38d04291948a32
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-06-11 15:51:11 +0200

    refactored segnalazioniweb

--- client/web/logic/segnalazioniweb.js
+++ client/web/logic/segnalazioniweb.js
@@ -1,27 +1,4 @@
- ///////// Ultime Segnalazioni ///////////
 
-Template.segnalazioniweb_refactored.created = function(){
-    depth = 0;
-};
-
-Template.segnalazioniweb_refactored.rendered = function () {
-    var now = moment();
-//    console.log("resetting last used to " + now.toString());
-    Session.set("lastUsed", now.toString());
-
-    $(document).ready(function(){
-        //$('.preloader-wrapper').pushpin({ top: $('.preloader-wrapper').offset().top });
-    });
-}
-
-//Template.smallFinesInHomePage.events({
-//    "click .NOOOOOOfine":function(){
-//        Session.set("dettaglio-web", this);
-//        $('#lista').hide();
-//        $('#dettaglio').show();
-//    }
-//});
-//
 //Template.smallFinesInHomePage.events({
 //    "click .fine":function(){
 //        Session.set("dettaglio-web", this);
@@ -31,7 +8,7 @@ Template.segnalazioniweb_refactored.rendered = function () {
 //});
 //
 
-Template.segnalazioniweb_refactored.helpers({
+Template.segnalazioniweb.helpers({
 
     fineToShow: function() {
         return Session.get("dettaglio-web");
