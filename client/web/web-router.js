be9115338419d667e896fb2b2a43486fd6b01973
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-06-12 00:09:39 +0200

    added facebook share

--- client/web/web-router.js
+++ client/web/web-router.js
@@ -40,6 +40,8 @@ if (!Meteor.isCordova) {    // legacy: needed?
     });
 
     Router.route('/web/segnalazione', function () {
+        $("#mipiace").attr("data-href", location.href);
+        console.log("like: %s", $("#mipiace").attr("data-href"));
         console.log("detail for %s", this.params.query._id);
         GAnalytics.pageview("/web/segnalazione");
         var fine = Fines.findOne({_id: this.params.query._id});
