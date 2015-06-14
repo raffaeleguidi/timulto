446b5d31f2eeb982eab473872884af10fb208f11
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-06-14 17:04:20 +0200

    fixed error in web/seo

--- server/api.js
+++ server/api.js
@@ -32,7 +32,7 @@ Router.route('/web/seo', function () {
     var title = "Segnalazione " + fine.address + " - TiMulto!"
     var description = fine.address + fine.text;
     var image = process.env.ROOT_URL + "api/image/" + fine._id;
-    var redirect = process.env.ROOT_URL + "web/segnalazione/?_id=" + fine._id;
+    var redirect = process.env.ROOT_URL + "web/segnalazione?_id=" + fine._id;
     this.response.write('<html><head>');
     this.response.write('<meta property="og:title" content="' + title + '" />');
     this.response.write('<meta property="og:description" content="' + description + '" />');
