7e602b609ed46db934e90fd3d9cd155a18cc1d3c
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-06-16 15:56:34 +0200

    moved test push to web

--- client/logic/chisiamo.js
+++ client/logic/chisiamo.js
@@ -17,23 +17,5 @@ Template.chisiamo.created = function(){
 Template.chisiamo.events({
     "click #shoot": function (event) {
         Router.go('/crea');
-    },
-    "click #sendmessage": function (event) {
-        Meteor.call("sendMessage", $("#msgto").val(), { title: $("#msgtitle").val(), message: $("#msgmessage").val()}, function(err, res) {
-            if (err) {
-                alert(err);
-            } else {
-                alert("messaggio inviato");
-            }
-        })
-    },
-    "click #sendtoalladmins": function (event) {
-        Meteor.call("sendMessageToAdmins", { title: $("#msgtitle").val(), message: $("#msgmessage").val()}, function(err, res) {
-            if (err) {
-                alert(err);
-            } else {
-                alert("messaggio inviato");
-            }
-        })
     }
 });
