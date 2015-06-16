5c8ed7c106915c7804dfea123d82a14d980de328
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-06-16 14:19:05 +0200

    added common function for fines and history

--- common.js
+++ common.js
@@ -6,5 +6,17 @@ Common = {
         paramName = paramName.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
         var regex = new RegExp("[\\?&]" + paramName + "=([^&#]*)"), results = regex.exec(location.search);
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    },
+    getFineIncludingHistory: function(fineId) {
+        var fine = Fines.findOne({_id: fineId});
+        if(!fine || fine == null) {
+            fine = FinesHistory.findOne({_id: fineId});
+            fine.live = false;
+        } else {
+            fine.live = true;
+        }
+        if (fine == null) {
+            console.log("fine not found in history for _id=%s", fineId);
+        }
     }
 }
